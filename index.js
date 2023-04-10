20dbe89a9e0c569e0e17720ecd87ef54a8d122ce
ClutchMasterFTW <clutchmasterftw@gmail.com>
2023-04-10 14:15:17 -0400

    added new content to homepage

--- index.js
+++ index.js
@@ -7,13 +7,15 @@ $(window).on('scroll', function() {
                               .css("flex-direction", "row");
 
         $("#header-logo").css("height", "75px")
-                         .css("margin-left", "10px");
+                         .css("margin-left", "10px")
+                         .css("margin-top", "0");
     } else {
         $("#header-container").css("height", "400px")
                               .css("position", "absolute")
                               .css("flex-direction", "column");
 
         $("#header-logo").css("height", "200px")
-                         .css("margin-left", "0px");
+                         .css("margin-left", "0px")
+                         .css("margin-top", "50px");
     }
 });
